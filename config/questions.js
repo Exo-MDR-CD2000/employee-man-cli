32a30f41a7a9977bdc52773750a410dcf125ff5e
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-06 19:06:02 -0700

    questions prompt draft

--- config/questions.js
+++ config/questions.js
@@ -15,14 +15,61 @@ const questions = [
             'Add a role',
             'Add an employee',
             'Update an employee role',
-            'Exit'
+            'Quit'
         ]
     },
     {
         type: 'input',
         name: 'departmentName',
-        message: 'What is the name of the department?'
-        
+        message: 'What is the name of the department?',
+    },
+    {
+        type: 'input',
+        name: 'roleTitle',
+        message: 'What is the title of the role?',
+    },
+    {
+        type: 'number',
+        name: 'roleSalary',
+        message: 'What is the salary of the role?',
+    },
+    {
+        type: 'input',
+        name: 'employeeFirstName',
+        message: 'What is the first name of the employee?',
+    },
+    {
+        type: 'input',
+        name: 'employeeLastName',
+        message: 'What is the last name of the employee?',
+    },
+    {
+        type: 'list',
+        name: 'employeeRoleId',
+        message: 'What is the role of the employee?',
+        choices: [],
+        when: (answers) => answers.mainMenu === 'Add an employee'
+    },
+    {
+        type: 'list',
+        name: 'employeeManagerId',
+        message: 'Who is the manager of the employee?',
+        choices: [],
+        when: (answers) => answers.mainMenu === 'Add an employee'
+    },
+    {
+        type: 'list',
+        name: 'employeeId',
+        message: 'Which employee would you like to update?',
+        choices: [],
+        when: (answers) => answers.mainMenu === 'Update an employee role'
+    },
+    {
+        type: 'list',
+        name: 'roleId',
+        message: 'What is the new role of the employee?',
+        choices: [],
+        when: (answers) => answers.mainMenu === 'Update an employee role'
     }
 ];
 
