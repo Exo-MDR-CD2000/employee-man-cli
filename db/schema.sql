3486d0caf979e96a523e9552355e2f37983f9056
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-08 13:02:59 -0700

    removed comments in schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,24 +6,19 @@ create database employee_manager_db;
 use employee_manager_db;
 
 create table department (
-    -- id int primary KEY,
     id int not null auto_increment primary KEY,
     name varchar(30) not null
 );
 
 create table role (
-    -- id int primary KEY,
     id int not null auto_increment primary KEY,
     title varchar(30) not null,
     salary int not null,
-    -- can also be set to decimal but it looks weird having a yearly salary with cents
     department_id int,
-    -- determine later if department_id needs to be set to 'not null'
     foreign key (department_id) references department(id)
 );
 
 create table employee (
-    -- id int primary key,
     id int not null auto_increment primary KEY,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
